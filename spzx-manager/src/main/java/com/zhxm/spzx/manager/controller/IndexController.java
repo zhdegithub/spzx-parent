0d59f027a40c20c19e0d0858810370f812f0660f
zhdegithub <wtzh0405@163.com>
2024-03-10 20:54:49 +0800

    登录验证码实现

--- spzx-manager/src/main/java/com/zhxm/spzx/manager/controller/IndexController.java
+++ spzx-manager/src/main/java/com/zhxm/spzx/manager/controller/IndexController.java
@@ -4,14 +4,13 @@ import com.atguigu.spzx.model.dto.system.LoginDto;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
+import com.atguigu.spzx.model.vo.system.ValidateCodeVo;
 import com.zhxm.spzx.manager.service.SysUserService;
+import com.zhxm.spzx.manager.service.ValidateCodeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户接口")
 @RestController
@@ -19,6 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @Autowired
     private SysUserService sysUserService;
+
+    @Autowired
+    private ValidateCodeService validateCodeService;
+
+    //生成图片验证码
+    @GetMapping(value = "/generateValidateCode")
+    public Result<ValidateCodeVo> generateValidateCode() {
+        ValidateCodeVo validateCodeVo = validateCodeService.generateValidateCode();
+        return Result.build(validateCodeVo,ResultCodeEnum.SUCCESS);
+    }
 
     //用户登录
     @Operation(summary = "登录的方法")
---
+++ spzx-manager/src/main/java/com/zhxm/spzx/manager/service/ValidateCodeService.java
@@ -0,0 +1,7 @@
+package com.zhxm.spzx.manager.service;
+
+import com.atguigu.spzx.model.vo.system.ValidateCodeVo;
+
+public interface ValidateCodeService {
+    ValidateCodeVo generateValidateCode();
+}
